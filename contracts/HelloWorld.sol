f2e4ce48db78e9e84323baace0434acf8efc484c
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2022-04-29 11:04:44 -0300

    Add HelloWorld.sol

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+contract HelloWorld{
+    function printHelloWorld() public pure returns(string memory){
+        return "Hello World";
+    }
+}
