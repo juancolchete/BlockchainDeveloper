77662a2cd66c01159299ff9238eb698e2dd1f072
juancolchete <38388534+juancolchete@users.noreply.github.com>
2022-11-14 09:00:50 -0300

    Add Variaveis.sol

---
+++ contracts/Variaveis.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+contract Variaveis{
+    address public conta = 0x241e6F0bE7A4F28DE8E0C9C8D1Cf79283BdA37dB;
+    uint public numeroInteiroNaoNegativo = 0;
+    int public numeroInteiro = -200;
+    bool public vaiChover = false;
+    bytes public tipoBytes = 'ola';
+    string public messagem = "Ola pessoal";
+}
