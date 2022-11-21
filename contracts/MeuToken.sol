9fce4127ed89deec0020fc081f3fff10ecd98950
juancolchete <38388534+juancolchete@users.noreply.github.com>
2022-11-20 21:30:23 -0300

    feat!: Add token supply

--- contracts/MeuToken.sol
+++ contracts/MeuToken.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MeuToken is ERC20 {
-    constructor() ERC20("MeuToken", "MTK") {}
+    constructor() ERC20("MeuToken", "MTK") {
+        _mint(msg.sender,1000000 * 10 ** 18);
+    }
 }
